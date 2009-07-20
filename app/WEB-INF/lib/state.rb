3f2b47e1de31bc654cf4e5ccfcc061b91e0378f8
Anthony Lai <aslai@MacBook-Pro.local>
2009-07-20 13:37:05 -0700

    Added fields for state

--- app/WEB-INF/lib/state.rb
+++ app/WEB-INF/lib/state.rb
@@ -2,4 +2,8 @@ require 'bumble.rb'
 class State
   include Bumble
   ds :distance, :sentence, :next_sentence, :next_random_mask, :test, :dictionary, :dictionary_array
+
+  def self.create
+    state = State.create! :sentence => "", :next_sentence => "", :next_random_mask => "", :test => false
+  end
 end
